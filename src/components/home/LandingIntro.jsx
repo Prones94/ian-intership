4e2a252301eb3a15c9b999c3cd46c78682c46292
Ian <ronesp.85@gmail.com>
2025-06-09 18:42:27 -0700

    installed aos library. Added animation to Landing.jsx, LandingIntro.jsx, and BrowseByCategory.jsx files

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -9,8 +9,8 @@ const LandingIntro = () => {
             <div className="feature-box f-boxed style-3">
               <i className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
-                <h4 className="">Set up your wallet</h4>
-                <p>
+                <h4 className="" data-aos="fade-up">Set up your wallet</h4>
+                <p data-aos="fade-up">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -22,8 +22,8 @@ const LandingIntro = () => {
             <div className="feature-box f-boxed style-3">
               <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
-                <h4 className="">Add your NFT's</h4>
-                <p>
+                <h4 className="" data-aos="fade-up">Add your NFT's</h4>
+                <p data-aos="fade-up">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -35,8 +35,8 @@ const LandingIntro = () => {
             <div className="feature-box f-boxed style-3">
               <i className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
-                <h4 className="">Sell your NFT's</h4>
-                <p>
+                <h4 className="" data-aos="fade-up">Sell your NFT's</h4>
+                <p data-aos="fade-up">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
